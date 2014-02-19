ff88d7d65dbf804a6c8920563889ad26f714ebcb
adamzy <rightcoset@gmail.com>
2014-02-19 16:33:21 +0800

    only preprocess necessary shot squences, improve performance

--- rmq.go
+++ rmq.go
@@ -1,6 +1,6 @@
 // Package `rmq` implements Bender-Farach Algorithm 
-// for RMQ (Range Mininum Query) problem.
-// Assume size of input array <= 2*64-1.
+// for RMQ (Range Minimum Query) problem.
+// Assume size of input array <= 2^63-1, since we use int64.
 // This should be OK for almost all cases.
 // For the algorithm details, check 
 //   Bender, Michael A., and Martin Farach-Colton. 
@@ -97,22 +97,21 @@ func log2(size int64) []int64 {
 
 // +-RMQ, the restricted RMQ
 func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
+	length := int64(len(A))
     // if `A` has length less than 2, do nothing.
-	if len(A) < 2 {
+	if length < 2 {
 		return func(x, y int64) (int64, int64) {
 			return 0, 0
 		}
-	}
-	length := int64(len(A))
+    }
 
 	// n is ceil(log(2))
 	n := int64(0)
 	for x := length; x != 0; x >>= 1 {
 		n++
 	}
-
-	//taken from graphics.stanford.edu/~seander/bithacks.html
-	// check if size is a power of 2
+    // taken from http://graphics.stanford.edu/~seander/bithacks.html
+	// check if length is a power of 2
 	if (length & (length - 1)) == 0 {
 		n--
 	}
@@ -136,7 +135,8 @@ func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
 	// pre[i] is the position of the first element in block i.
 	pre := make([]int64, num)
 
-	// B[i][j][k] is the optimal position in block i, from position j to k.
+	// B[i][j][k] is the optimal position in block i,
+    // between position j and  k.
 	B := make([][][]int64, num)
 
 	// for sparse table algorithm
@@ -144,7 +144,7 @@ func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
 	D := make([]int64, num) // value
 
 	var i, j int64
-	processer := processBlock(size) //, 1<<uint64(size-1))
+	processer := processBlock(size)
 
 	var pos int64
 	for pos = 0; pos+size < length; pos += size {
@@ -155,14 +155,11 @@ func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
 		pre[i] = pos
 		B[i] = processer(A[pos : pos+size])
 		C[i] = B[i][0][size-1] + pos
-		//D[i] = E[i][0][size-1]
 		D[i] = A[C[i]]
-		//fmt.Println("_", A[pos:pos+size], B[i][0][size-1], D[i])
 		i++
 	}
 	if pos != length {
-		// pos < length
-		// i = num-1
+		// in this case, pos < length and i = num-1
 		for j = 0; j < length-pos; j++ {
 			loc[pos+j] = i
 			res[pos+j] = j
@@ -170,19 +167,9 @@ func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
 		pre[i] = pos
 		B[i] = processer(A[pos:length])
 		C[i] = B[num-1][0][length-pos-1] + pos
-		//D[num-1] = E[num-1][0][length-pos-1]
 		D[i] = A[C[num-1]]
 	}
 	tabler := st(D)
-
-	/*fmt.Println(C)*/
-	/*fmt.Println(D)*/
-	//for i:=int64(0);i<num;i++ {
-	//for j:=i; j<num; j++ {
-	//a, b := tabler(i,j)
-	//fmt.Println(i,j,a,b)
-	//}
-	//}
 
 	return func(x, y int64) (int64, int64) {
 		// always assume 0 <= x <= y < length
@@ -193,21 +180,16 @@ func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
 		ur, vr := res[x], res[y]
 		up, vp := pre[u], pre[v]
 
-        //println("~", x, y, ":", u, v, ur, vr, up, vp)
 		if u == v {
-			//return B[u][ur][vr], E[u][ur][vr]
 			pos := up + B[u][ur][vr]
 			return pos, A[pos]
 		} else {
 			// u < v
 			p1 := up + B[u][ur][size-1]
-			//v1 := E[u][ur][size-1]
 			v1 := A[p1]
 			p2 := vp + B[v][0][vr]
-			//v2 := E[v][0][vr]
 			v2 := A[p2]
 
-            //println("v1, p1, v2, p2: ", v1, p1, v2, p2)
 			var vv, pp int64
 			if v1 <= v2 {
 				vv = v1
@@ -219,29 +201,28 @@ func ResRMQ(A []int64) func(x, y int64) (p, v int64) {
 
 			if v-u > 1 {
 				p3, v3 := tabler(u+1, v-1)
-                //fmt.Println(D)
-                //println("v3, p3: ", v3, p3)
 				if v3 < vv {
 					vv = v3
 					pp = C[p3]
 				}
-				//println(v1, v2, v3)
 			}
 			return pp, vv
 		}
 	}
 }
 
+// Convert a +-1 sequence to int, e.g.
+// [1 2 3 2 1 0 -1 -2 -1 0 1] => 0-1 sequence 1100000111 => integer
 func arraytoint(A []int64) int64 {
-	if len(A) > 64 {
-		panic("Overflow")
+	if len(A) > 63 {
+		panic("We cannot handle such long sequence")
 	}
 	var v int64
 	for i := len(A) - 1; i > 0; i-- {
 		if A[i] == A[i-1]+1 {
 			v |= 1
 		} else if A[i] != A[i-1]-1 {
-			panic("Not +-1 array")
+			panic("Not a +-1 sequence")
 		}
 		v <<= 1
 	}
@@ -249,21 +230,26 @@ func arraytoint(A []int64) int64 {
 }
 
 func processBlock(size int64) func([]int64) [][]int64 {
-        // num = 2^(size)
+    // number of all possible +-1 sequence begin with 0
 	num := int64(1 << uint64(size))
 	a := make([][][]int64, num)
-	for i := int64(0); i < num; i++ {
-		a[i] = dynamic(i, size)
-	}
+	//for i := int64(0); i < num; i++ {
+		//a[i] = dynamic(i, size)
+	//}
 	return func(A []int64) [][]int64 {
 		v := arraytoint(A)
+        // if a[v] hasn't been computed before, then compute it.
+        // otherwise, use the cached result.
+        if a[v] == nil {
+            a[v] = dynamic(v, size)
+        }
 		return a[v]
 	}
 }
 
 func dynamic(n, size int64) [][]int64 {
-	if size > 64 {
-		panic("Cannot")
+	if size > 63 {
+		panic("We cannot handle such long sequence")
 	}
 	t := make([][]int64, size) //t[i][j] = position with min value in A[i,j]
 	m := make([][]int64, size) //m[i][j] = min value in A[i,j]
